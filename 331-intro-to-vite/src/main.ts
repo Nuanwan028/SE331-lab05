bf6d31a993579395f9033f9ea8f8b06d0d1c3b72
kmj <nuanwan_w@cmu.ac.th>
2025-08-01 00:37:34 +0700

    add Vercel Speed Insights

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -12,6 +12,9 @@ const app = createApp(App)
 import { inject } from '@vercel/analytics'
 inject()
 
+import { injectSpeedInsights } from '@vercel/speed-insights'
+injectSpeedInsights()
+
 app.use(createPinia())
 app.use(router)
 
